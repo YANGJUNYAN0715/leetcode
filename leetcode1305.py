5e593043b894e46f6400289cc84720a2d3d5f69e
Junyan Yang <1310204834@qq.com>
2022-05-01 14:36:06 +0800

    2022.5.1

---
+++ leetcode1305.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+        res = []
+        self.preTraverse(root1, res)
+        self.preTraverse(root2, res)
+        res.sort()
+        return res
+
+    def preTraverse(self, root: TreeNode, list: List[int]):
+        if root is not None:
+            list.append(root.val)
+            self.preTraverse(root.left,list)
+            self.preTraverse(root.right,list)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    root1 = TreeNode(1,None,TreeNode(8))
+    root2 = TreeNode(8,TreeNode(1))
+    print(s.getAllElements(root1,root2))
