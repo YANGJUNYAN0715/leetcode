35cdf3718a8668fc27dac9a0dfb9f8a5a0fde896
Junyan Yang <1310204834@qq.com>
2022-05-08 18:20:14 +0800

    2022.5.8

--- leetcode442.py
+++ leetcode442.py
@@ -3,13 +3,17 @@ from typing import List
 
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        a = set()
-        res = []
+        # a = set()
+        # res = []
+        # for i in range(len(nums)):
+        #     if nums[i] in a:
+        #         res.append(nums[i])
+        #     a.add(nums[i])
+        # return res
         for i in range(len(nums)):
-            if nums[i] in a:
-                res.append(nums[i])
-            a.add(nums[i])
-        return res
+            while nums[i] != nums[nums[i] - 1]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+        return [num for i, num in enumerate(nums) if num - 1 != i]
 
 
 if __name__ == '__main__':
